298bedcbd74b0bdbacb1bd1817e2c3310ba342d4
TheShamRio <xsham@inbox.ru>
2024-10-24 14:41:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.artspaceapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Empty activity for now
+    }
+}
